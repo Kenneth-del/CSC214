1870cf3165111c0685600c74414c9b742302f9a3
jbatista1998 <61163162+jbatista1998@users.noreply.github.com>
2020-04-17 16:13:20 -0400

    Add files via upload

--- src/group2/MyConnection.java
+++ src/group2/MyConnection.java
@@ -4,15 +4,15 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 public class MyConnection {
 
-	//Defines connection to AWS mySQL database
+	//Defines connection to AWS mySQL database 
     public static Connection getConnection() {
 
     	//Database url
-        String url = "jdbc:mysql://localhost";
+        String url = "jdbc:mysql://localhost/mydb";
         //Database username (Only used for the program)
-        String user = "java";
+        String user = "root";
         //Database password
-        String password = "Java123!";
+        String password = "root123";
         //Creates connection object
         Connection con = null;
         try {
@@ -20,14 +20,14 @@ public class MyConnection {
             Class.forName("com.mysql.jdbc.Driver");
             //Attempts to connect to database using defined credentials
             con = DriverManager.getConnection(url, user, password);
-            //Here for Debub Purpsoses
+            //Here for Debub Purpsoses 
             System.out.println("Connection created");
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
         //            TO DO: Find in program to close connection
-        //            con.close();
-        //            System.out.println("Connection closed");
+        //            con.close();  
+        //            System.out.println("Connection closed");  
         return con;
     }
 }
